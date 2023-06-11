845dc3f036a5e0d68f0a3e88a97e874cfe6f3d2d
Théo BOURDIER <theobourdier1@gmail.com>
2023-06-11 13:52:42 +0200

    Format

--- utils/formatDate.mjs
+++ utils/formatDate.mjs
@@ -1,15 +1,13 @@
 function formatDate(date) {
   let d = new Date(date),
-      month = '' + (d.getMonth() + 1),
-      day = '' + d.getDate(),
-      year = d.getFullYear();
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
 
-  if (month.length < 2) 
-      month = '0' + month;
-  if (day.length < 2) 
-      day = '0' + day;
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
 
-  return [day, month, year].join('_');
+  return [day, month, year].join("_");
 }
 
 export default formatDate;
